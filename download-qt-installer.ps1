c00e96ecee4ee1919dfbc9494c24ce4b16f6b88c
ChristianReese <christian.g.reese96@gmail.com>
2019-07-22 10:13:20 -0500

    Delete download-qt-installer.ps1

--- download-qt-installer.ps1
+++
@@ -1,2 +0,0 @@
-$client = new-object System.Net.WebClient
-$client.DownloadFile("http://download.qt-project.org/official_releases/qt/5.9/5.9.7/qt-opensource-windows-x86-5.9.7.exe", "install-qt-5.9.7.exe")
