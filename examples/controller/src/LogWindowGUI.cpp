336e50a4bba70fdaac8802228b9bc60960faef3f
ChristianReese <christian.g.reese96@gmail.com>
2019-08-23 12:57:08 -0500

    Fix destructor order/dependency issue
    LogWindowGUI was depending on the main network model which was sometimes destroyed when accessed, causing a crash. LogWindowGUI no longer depends on it, and it is up to the code using LogWindowGUI to connect it to output streams. This is a better design and fixes the crash.

--- examples/controller/src/LogWindowGUI.cpp
+++ examples/controller/src/LogWindowGUI.cpp
@@ -28,27 +28,14 @@
 
 #include <sstream>
 
-LogWindowGUI::LogWindowGUI(QWidget* parent, RDMnetNetworkModel* model) : QDialog(parent), model_(model)
+LogWindowGUI::LogWindowGUI(QWidget* parent) : QDialog(parent)
 {
   ui.setupUi(this);
 
   connect(this, SIGNAL(appendText(const QString&)), this, SLOT(processAppendText(const QString&)), Qt::AutoConnection);
   connect(this, SIGNAL(clearText()), this, SLOT(processClearText()), Qt::AutoConnection);
 
-  if (model != NULL)
-  {
-    model->addCustomLogOutputStream(this);
-  }
-
   setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
-}
-
-LogWindowGUI::~LogWindowGUI()
-{
-  if (model_ != NULL)
-  {
-    model_->removeCustomLogOutputStream(this);
-  }
 }
 
 LogOutputStream& LogWindowGUI::operator<<(const std::string& str)
