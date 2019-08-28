8f35604b5fce82dfe0a5ea91897251f16cc74a6d
ChristianReese <christian.g.reese96@gmail.com>
2019-08-28 15:04:58 -0500

    Printing push output for diagnostic info

--- tools/ci/publish_docs.sh
+++ tools/ci/publish_docs.sh
@@ -93,7 +93,7 @@ if [ -d "docs/head" ] && [ -f "docs/head/index.html" ]; then
       # Force push to the remote gh-pages branch.
       # The ouput is redirected to /dev/null to hide any sensitive credential data
       # that might otherwise be exposed.
-      git push --force "https://svc-etclabs:${SVC_ETCLABS_PASSWORD}@${GH_REPO_REF}" > /dev/null 2>&1
+      git push --force "https://svc-etclabs:${SVC_ETCLABS_PASSWORD}@${GH_REPO_REF}"
     else
       echo 'No documentation changes. Doing nothing.'
     fi
