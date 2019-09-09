1e16fcecf05dbc48e9f0ce70e028e2c83eb9181d
Sam Kearney <samuelmkearney@gmail.com>
2019-09-09 16:13:17 -0500

    Update version files for RDMnet build 0.3.0.1

--- include/rdmnet/version.h
+++ include/rdmnet/version.h
@@ -46,16 +46,16 @@
  *  @{
  */
 #define RDMNET_VERSION_MAJOR 0 /*!< The major version. */
-#define RDMNET_VERSION_MINOR 2 /*!< The minor version. */
+#define RDMNET_VERSION_MINOR 3 /*!< The minor version. */
 #define RDMNET_VERSION_PATCH 0 /*!< The patch version. */
-#define RDMNET_VERSION_BUILD 7 /*!< The build number. */
+#define RDMNET_VERSION_BUILD 1 /*!< The build number. */
 /*!@}*/
 
 /*! \name RDMnet version strings
  *  @{
  */
-#define RDMNET_VERSION_STRING "0.2.0.7"
-#define RDMNET_VERSION_DATESTR "2019-05-29"
+#define RDMNET_VERSION_STRING "0.3.0.1"
+#define RDMNET_VERSION_DATESTR "09.Sep.2019"
 #define RDMNET_VERSION_COPYRIGHT "Copyright 2019 ETC Inc."
 #define RDMNET_VERSION_PRODUCTNAME "RDMnet"
 /*!@}*/
