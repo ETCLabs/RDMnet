e0d99a0f3d3d335b44b0a8983a61094572710d4c
Sam Kearney <samuelmkearney@gmail.com>
2019-11-01 16:33:54 -0500

    Remove hardcoded binaries

--- external/libGadget/x64/lib/cmake/GadgetDLL/GadgetDLLConfig.cmake
+++
@@ -1,7 +0,0 @@
-include(FindPackageHandleStandardArgs)
-set(${CMAKE_FIND_PACKAGE_NAME}_CONFIG ${CMAKE_CURRENT_LIST_FILE})
-find_package_handle_standard_args(GadgetDLL CONFIG_MODE)
-
-if(NOT TARGET GadgetDLL::GadgetDLL)
-    include("${CMAKE_CURRENT_LIST_DIR}/GadgetDLLTargets.cmake")
-endif()
