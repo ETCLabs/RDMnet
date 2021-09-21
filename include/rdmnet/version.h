ad2d8384b0a16729c9a50460bf0fd9e64198cbfd
GitLab CI <noreply.gitlab@etcconnect.com>
2021-09-21 18:32:49 +0000

    [skip ci] Update version files for build 0.4.0.3

--- include/rdmnet/version.h
+++ include/rdmnet/version.h
@@ -39,7 +39,7 @@
 #define RDMNET_VERSION_MAJOR 0 /**< The major version. */
 #define RDMNET_VERSION_MINOR 4 /**< The minor version. */
 #define RDMNET_VERSION_PATCH 0 /**< The patch version. */
-#define RDMNET_VERSION_BUILD 2 /**< The build number. */
+#define RDMNET_VERSION_BUILD 3 /**< The build number. */
 /**
  * @}
  */
@@ -48,8 +48,8 @@
  * @name RDMnet version strings
  * @{
  */
-#define RDMNET_VERSION_STRING "0.4.0.2" /**< The version number as a string. */
-#define RDMNET_VERSION_DATESTR "27.Jul.2021" /**< The date this version was created. */
+#define RDMNET_VERSION_STRING "0.4.0.3" /**< The version number as a string. */
+#define RDMNET_VERSION_DATESTR "21.Sep.2021" /**< The date this version was created. */
 #define RDMNET_VERSION_COPYRIGHT "Copyright 2021 ETC Inc." /**< A copyright string for this library. */
 #define RDMNET_VERSION_PRODUCTNAME "RDMnet" /**< The name of the library as a string. */
 /**
