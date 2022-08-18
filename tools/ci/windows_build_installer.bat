897142fc2cca0cb8db12fc90f96d5d6bd67741b9
Christian Reese <christian.reese@etcconnect.com>
2022-08-18 10:48:48 -0500

    Update Windows pipelines to VS 2019

--- tools/ci/windows_build_installer.bat
+++ tools/ci/windows_build_installer.bat
@@ -1,4 +1,4 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat" %VCVARSALL_PLATFORM%
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" %VCVARSALL_PLATFORM%
 
 MSBuild.exe -property:Configuration=Release -property:Platform=%ARTIFACT_TYPE% .\tools\install\windows\RDMnetInstall_%ARTIFACT_TYPE%.wixproj
 IF %ERRORLEVEL% NEQ 0 ( EXIT /B %ERRORLEVEL% )
