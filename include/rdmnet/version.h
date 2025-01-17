327b5b1048474400479b48238bf63f89517b15a3
ETC CI <noreply.ci@etcconnect.com>
2025-01-17 17:30:02 +0000

    [skip ci] Update files for official build v1.0.0.6

--- include/rdmnet/version.h
+++ include/rdmnet/version.h
@@ -39,7 +39,7 @@
 #define RDMNET_VERSION_MAJOR 1 /**< The major version. */
 #define RDMNET_VERSION_MINOR 0 /**< The minor version. */
 #define RDMNET_VERSION_PATCH 0 /**< The patch version. */
-#define RDMNET_VERSION_BUILD 5 /**< The build number. */
+#define RDMNET_VERSION_BUILD 6 /**< The build number. */
 /**
  * @}
  */
@@ -48,9 +48,9 @@
  * @name RDMnet version strings
  * @{
  */
-#define RDMNET_VERSION_STRING "1.0.0.5" /**< The version number as a string. */
-#define RDMNET_VERSION_DATESTR "12.Jan.2024" /**< The date this version was created. */
-#define RDMNET_VERSION_COPYRIGHT "Copyright 2024 ETC Inc." /**< A copyright string for this library. */
+#define RDMNET_VERSION_STRING "1.0.0.6" /**< The version number as a string. */
+#define RDMNET_VERSION_DATESTR "17.Jan.2025" /**< The date this version was created. */
+#define RDMNET_VERSION_COPYRIGHT "Copyright 2025 ETC Inc." /**< A copyright string for this library. */
 #define RDMNET_VERSION_PRODUCTNAME "RDMnet" /**< The name of the library as a string. */
 /**
  * @}
